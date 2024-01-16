dcfa6278e8eb5801003c43637e2abc6832584359
mo92othman <mo92othman@gmail.com>
2024-01-16 17:37:15 +0100

    the user is able to edit their profile info

--- src/components/userProfile/UserProfile.js
+++ src/components/userProfile/UserProfile.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import Avatar from './Avatar';
 import { useAppState } from '../../AppStateContext';
+import { Link } from 'react-router-dom';
 
 function UserProfile() {
   const { state } = useAppState();
   const { name, avatarUrl } = state.user;
 
   return (
-    <div className="flex items-center justify-between bg-purple-300 p-4 rounded-lg">
-      <div className="flex items-center">
-        <h2 className="text-xl font-bold text-white">Hi {name}!</h2>
+    <Link to="/profile">
+      <div className=" group flex items-center justify-between bg-purple-300 p-4 rounded-lg transition duration-100 ease-in-out hover:bg-purple-400">
+        <div className="flex items-center">
+          <h2 className="text-2xl font-bold text-white transition duration-900 ease-in-out  group-hover:animate-bounce group-hover:text-3xl">
+            Hi {name}!
+          </h2>
+        </div>
+        <div className=" transition-transform duration-100 ease-in-out transform group-hover:animate-bounce">
+          <Avatar svgString={avatarUrl} />
+        </div>
       </div>
-      <div className="w-12 h-12 overflow-hidden rounded-full">
-        {/* <div dangerouslySetInnerHTML={{ __html: avatarUrl }} /> */}
-      </div>
-      <Avatar svgString={avatarUrl} />
-    </div>
+    </Link>
   );
 }
 
